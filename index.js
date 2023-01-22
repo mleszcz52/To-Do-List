712cca199ddf15641777ce8d9bac980f2e96f9d3
mleszcz52 <122688067+mleszcz52@users.noreply.github.com>
2023-01-22 16:14:33 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
 function add() {
-  const newToDo = document.createElement("li");
-  const content = document.getElementById("toDoInput").value;
-  const toDoList = document.getElementById("toDoList");
+  const newElement = document.createElement('li');
+  const input = document.getElementById('input').value;
+  const list = document.getElementById('myList');
+  newElement.textContent = input;
 
-  newToDo.textContent = content;
-  if (content === '') {
-    alert("You must write something!");
-  } else {
-    toDoList.appendChild(newToDo);
-    newToDo.addEventListener("click", function(event){ 
-      if (event.target.classList.contains("done")) {
-        event.target.classList.remove("done")    
-      }
-      else {
-        event.target.classList.add("done")
-      }
-    });
+  if (input === '') {
+    alert('You need to write something!');
+    return;
+  }
+  else {
+    list.appendChild(newElement);
+  }
+  document.getElementById("input").value = "";
+
+  let span = document.createElement('span');
+  let txt= document.createTextNode('delete');
+  span.classList.add("material-symbols-outlined", "deleteBtn")
+  span.appendChild(txt);
+  newElement.appendChild(span);
+
+  for(i=0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+}};
+
+let close = document.getElementsByClassName("deleteBtn");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    let div = this.parentElement;
+    div.style.display = "none";
   }
 }
 
-document.getElementById("addBtn").addEventListener("click", add);
-const toDoInput = document.getElementById("toDoInput");
-toDoInput.addEventListener("keyup", function(event){
-  if (event.key === "Enter") {
-    add();
+var myNodeList = document.getElementsByTagName('LI');
+var i;
+for (i = 0; i < myNodeList.length; i++) {
+  let span = document.createElement("span")
+  let txt = document.createTextNode("delete");
+  span.classList.add("material-symbols-outlined", "deleteBtn")
+  span.appendChild(txt);
+  myNodeList[i].appendChild(span);
+}
+
+
+
+let mylist = document.querySelectorAll('ul');
+mylist[i].addEventListener("click", function (ev) {
+  if (ev.target.tagName === "LI") {
+    ev.target.classList.toggle("done");
   }
-});
+}, false);
+
+document.getElementById('addBtn').addEventListener('click', add);
